4c4aca5fd771bef522c82a0efc3b818e44db8bdc
rodasenyew <rodasenyew229@gmail.com>
2025-06-02 21:42:33 -0900

    added local storage

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,11 +1,17 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 export const CartContext = createContext();
-
 export const useCart = () => useContext(CartContext);
 
 export const CartProvider = ({ children }) => {
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState(() => {
+        const storedCart = localStorage.getItem("cart");
+        return storedCart?JSON.parse(storedCart):[];
+    });
+
+    useEffect(() => {
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }, [cart]);
 
     const addToCart = (product) => {
         setCart((prev) => {
