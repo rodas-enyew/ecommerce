f840e3fb7353441ffbdc9d76f1aad63e94427acc
rodasenyew <rodasenyew229@gmail.com>
2025-06-10 01:45:41 -0900

    Fixed the Leak

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,7 @@
 // Import express
 const express = require('express');
-const mongoose = require("mongoose");
-const productRoutes = require('./routes/productRoutes'); // Import routes
+const fetch = require("node-fetch")// to fetch from external API
 require('dotenv').config(); // loads the .env file
-const orderRoutes = require("./routes/orderRoutes");
-
-app.use("api/orders", orderRoutes);
 
 const{ getProfile } = require("@yayawallet/node-sdk");
 
@@ -32,22 +28,3 @@ const app = express();
 // Middleware to parse incoming JSON requests
 app.use(express.json());
 
-// Use routes - everything under /api/products goes to productRoutes
-app.use('/api/products', productRoutes);
-
-console.log ("Mongo URI from.env:", process.env.MONGO_URI);//connect to mongodb 
-
-mongoose.connect(process.env.MONGO_URI, {
-})
-.then(()=> {
-    console.log("Connected to MongoDB!");
-
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-})
-.catch((error) => {
-    console.error("Failed To connect to MongoDB: ", error.message);
-});
